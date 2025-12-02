d7cc864f12d063f9888338ccf323ce1563dc347d
anderson-augusto <anderson.lopes@sptech.school>
2025-12-01 23:08:37 -0300

    ?²

--- web-data-viz/src/routes/leitura.js
+++
@@ -1,8 +0,0 @@
-// src/routes/leituraRoutes.js
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/leituraController');
-
-router.post('/', ctrl.inserirLeitura); 
-
-module.exports = router;
--- web-data-viz/src/routes/talhao.js
+++
@@ -1,9 +0,0 @@
-// src/routes/talhaoRoutes.js
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/talhaoController');
-
-router.get('/:id/leituras/realtime', ctrl.leiturasRealtime);
-router.get('/:id/leituras/historico', ctrl.mediaHistorica);
-
-module.exports = router;
